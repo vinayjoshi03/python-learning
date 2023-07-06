1e3fee589fbc7da9b7b95119e9708c5e82363d27
Vinay Joshi <vinayjoshi2@gmail.com>
2023-07-06 16:35:15 +0530

    Data types

---
+++ python-basics/datatypes.py
@@ -0,0 +1,20 @@
+print("\n")
+print("#### Data type introduction #######")
+
+print("#1. integer - 1+1 OP: ", 1+1, " Type: ", type(1+1))
+print("#2. negative integer - 4-1 is OP: ", 4-1, " Type: ", type(4-1));
+print("#3. float - 3/2 OP: ", 3/2, " Type: ", type(3/2));
+print("---------------------------------------------------");
+print("#4. float 9.9+1.1 is 11.0 which looks like integer but its float OP: ", 9.9+1.1, " Type: ", type(9.9+1.1));
+print("---------------------------------------------------");
+print("#5. double multiply - 2 ** 2 = 4 OP: ", 2 ** 2);
+print("#6. double multiply - 2 ** 3 = 8 OP: ", 2 ** 3);
+
+print("#7. double multiply - 3 ** 2 = 9 OP: ", 3 ** 2);
+print("#8. double multiply - 3 ** 3 = 27 OP: ", 3 ** 3);
+
+print("---------------------------------------------------");
+print("#9. double divided by - 2 // 2 OP: ", 2 // 2);
+print("#10. double divided by - 2 // 4 OP: ", 2 // 4);
+print("#11. double divided by - 100 / 2 OP: ", 100 / 2);
+print("#12. double divided by - 100 // 2 OP: ", 100 // 2);
